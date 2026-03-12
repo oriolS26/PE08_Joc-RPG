59b84596d568ccac5a32a8fe9180af8faa181021
Oriol Sarda <alu.oriol.sarda@mataro.epiaedu.cat>
2026-03-12 19:20:05 +0100

    Metode constructor personatge

--- personatge.java
+++ personatge.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class personatge {
     private String nom;
-    private int edate;
+    private int edat;
     private int forca;
     private int destresa;
     private int constitucio;
@@ -14,9 +14,26 @@ public class personatge {
     private double salutMax;
     private double manaMax;
 
-    private int energia;
+    private int energia = 0;
+    private boolean habilitatEspecial = false;
     private arma armaEquipada;
     private ArrayList<arma> inventari = new ArrayList<>();
     private boolean defensat = false;
 
+    public personatge (String nom, int edat, int forca, int destresa, int constitucio, int inteligencia, int saviesa, int carisma) {
+        this.nom = nom;
+        this.edat = edat;
+        this.forca = forca;
+        this.destresa= destresa;
+        this.constitucio = constitucio;
+        this.inteligencia = inteligencia;
+        this.saviesa = saviesa;
+        this.carisma = carisma;
+        this.salutMax = constitucio * 50;
+        this.manaMax = inteligencia * 30;
+        this.salut = salutMax;
+        this.mana = manaMax;
+    
+        inventari.add(new arma("Espasa Llarga", tipusarma.ESPASA, 15, false));
+    }
 }
