fc031fa1a4e0bcf5d7475fb648364b98928356ae
Oriol Sarda <alu.oriol.sarda@mataro.epiaedu.cat>
2026-03-12 19:04:16 +0100

    Armas acabades, i atributs personatge

---
+++ personatge.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class personatge {
+    private String nom;
+    private int edate;
+    private int forca;
+    private int destresa;
+    private int constitucio;
+    private int inteligencia;
+    private int saviesa;
+    private int carisma;
+    private double salut;
+    private double mana;
+    private double salutMax;
+    private double manaMax;
+
+    private int energia;
+    private arma armaEquipada;
+    private ArrayList<arma> inventari = new ArrayList<>();
+    private boolean defensat = false;
+
+}
